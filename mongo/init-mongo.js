b5e1c221ed7cda18f2577a11eea1572ed2071f76
100495854 <100495854@alumnos.uc3m.es>
2025-10-21 18:26:49 +0200

    tankeada

--- mongo/init-mongo.js
+++ mongo/init-mongo.js
@@ -1,6 +1,28 @@
-db = db.getSiblingDB(process.env.APP_DB || "movies_db");
-db.createUser({
-    user: process.env.APP_USER || "liang",
-    pwd: process.env.APP_PWD || "liang",
-    roles: [{ role: "readWrite", db: process.env.APP_DB || "movies_db" }]
-});
+const dbName = process.env.APP_DB || "movies_db";
+db = db.getSiblingDB(dbName);
+
+function ensureUser(user, pwd) {
+  try {
+    print(`[init-mongo.js] Creating or ensuring user '${user}' in DB '${dbName}' ...`);
+    db.createUser({
+      user: user,
+      pwd: pwd,
+      roles: [{ role: "readWrite", db: dbName }]
+    });
+    print(`[init-mongo.js] User '${user}' created successfully.`);
+  } catch (e) {
+    if (e.code === 11000) {
+      print(`[init-mongo.js] User '${user}' already exists, skipping.`);
+    } else {
+      print(`[init-mongo.js] Warning while creating user '${user}': ${e}`);
+    }
+  }
+}
+
+const envUser = process.env.APP_USER || "liang";
+const envPwd  = process.env.APP_PWD  || "liang";
+ensureUser(envUser, envPwd);
+
+ensureUser("jibm23", "nichinonibastardo");
+
+print(`[init-mongo.js] Initialization complete for DB '${dbName}'.`);
